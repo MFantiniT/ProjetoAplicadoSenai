39862ce9d373d739a8113c462cfdab38ae013620
MFantiniT <matheusfantini@gmail.com>
2023-06-29 19:27:21 -0300

    continuando

--- templates/header.php
+++ templates/header.php
@@ -53,7 +53,7 @@
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="dashboard.php">
                   Dashboard
                 </a>
               </li>
